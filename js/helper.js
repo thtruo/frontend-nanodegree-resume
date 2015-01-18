e210fd3cea00cd4ecc9d5ab054ba3f72df667d31
Thomas Truongchau <ttruongchau@gmail.com>
2015-01-17 16:44:02 -0800

    modified infoWindow for displayed google maps

--- js/helper.js
+++ js/helper.js
@@ -166,12 +166,12 @@ function initializeMap() {
     // or hover over a pin on a map. They usually contain more information
     // about a location.
     var infoWindow = new google.maps.InfoWindow({
-      content: name
+      content: '<h5>' + name + '</h5>'
     });
 
     // hmmmm, I wonder what this is about...
     google.maps.event.addListener(marker, 'click', function() {
-      // your code goes here!
+      infoWindow.open(map, marker);
     });
 
     // this is where the pin actually gets added to the map.
